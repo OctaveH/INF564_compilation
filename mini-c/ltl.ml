d4e7f00cc2adee75b879961be8727d695b8c6841
Octave Hazard <octave.hazard@yahoo.fr>
2020-03-09 18:23:45 +0100

    Still empty Ltl

--- mini-c/ltl.ml
+++ mini-c/ltl.ml
@@ -1,3 +1,11 @@
+(* 1.1 - Construction du graphe d'interférence *)
+
+type arcs = { prefs: Register.set; intfs: Register.set }
+type igraph = arcs Register.map
+
+let make (li:Ertltree.live_info Label.map) : igraph =
+  let ig = Register.M.empty in
+  ig
 
 let program (ertl_file:Ertltree.file) : Ltltree.file =
   {funs = []}
--- mini-c/ltl.mli
+++ mini-c/ltl.mli
@@ -1,2 +1,6 @@
+type arcs = { prefs: Register.set; intfs: Register.set }
+type igraph = arcs Register.map
+
+val make: Ertltree.live_info Label.map -> igraph
 
 val program: Ertltree.file -> Ltltree.file
