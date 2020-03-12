4a201cb1d7f0de3ce2f1185cfaf01a6da5f65ceb
Vialard <fanny.vialard@free.fr>
2020-03-12 14:26:11 +0100

    Optimisation appel terminaux Ertltreelive.ml/mli et mainlive peuvent remplacer ertltree et main pour avoir l'impression de la liveness

--- mini-c/test.c
+++ mini-c/test.c
@@ -7,5 +7,16 @@
 
 /*** listes circulaires doublement chaînées ***/
 
-int f(int x) {}
-int main() { f(); }
+//int f(int x) {}
+//int main() { f(); }
+
+int f(int x, int y, int z, int t) {
+  if (!x) return 10;
+  putchar(x);
+  return f(y, z, t, x);
+}
+
+int main() {
+  putchar(f('A', 'B', 'C', 0));
+  return 0;
+}
